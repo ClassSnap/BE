3a2fd0832822f006f10e1dac48afd1a9af8b8a0b
karenjli <karenjy.li@gmail.com>
2019-11-12 10:22:06 -0600

    fixed seed

--- data/seeds/008-rating.js
+++ data/seeds/008-rating.js
@@ -7,25 +7,36 @@ exports.seed = function(knex) {
       return knex("ratings").insert([
         {
           id: 1,
+          rating: 5,
           questionId: 1,
           learnerParentId: 1,
           classId: 1,
-          completed: false,
+          completed: true
         },
         {
           id: 2,
+          rating: 3,
           questionId: 1,
           learnerParentId: 2,
           classId: 1,
-          completed: false,
+          completed: true
         },
         {
           id: 3,
+          rating: 2,
           questionId: 2,
-          learnerParentId: 3,
-          classId: 2,
-          completed: false,
+          learnerParentId: 2,
+          classId: 1,
+          completed: true
         },
+        {
+          id: 4,
+          rating: 1,
+          questionId: 2,
+          learnerParentId: 1,
+          classId: 1,
+          completed: true
+        }
       ]);
     });
 };
